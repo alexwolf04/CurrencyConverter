0769d6cd01ac3eccf095169c2d6d8641eec143cc
alexwolf04 <alexwolf151515@gmail.com>
2025-07-17 19:07:55 -0500

    Initial commit

---
+++ src/RunnableImpl.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Alex Wolf
+ */
+public class RunnableImpl implements Runnable {
+
+    public RunnableImpl() {
+    }
+
+    @Override
+    public void run() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
